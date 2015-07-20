0daa894898fb6aea6f2de46966e56423926c5c9c
ogencoglu <oguzhangencoglu90@gmail.com>
2015-07-21 00:19:25 +0300

    comments changed

--- code/get_file_locs.py
+++ code/get_file_locs.py
@@ -2,15 +2,15 @@
 Author         : Oguzhan Gencoglu
 Contact        : oguzhan.gencoglu@tut.fi
 Created        : 18.07.2015
-Latest Version : 18.07.2015
+Latest Version : 20.07.2015
 '''
 
 import glob
 import os
 
 def get_file_locs(directory, extension):
-    # Returns a list of file names of a given extension under a directory
-    # E.g. get_file_locs(images, "png")
+    # Returns a list of file paths of a given extension under a directory
+    # E.g. get_file_locs('data\images', "png")
     
     files = []
     for root, dirnames, filenames in os.walk(directory):
